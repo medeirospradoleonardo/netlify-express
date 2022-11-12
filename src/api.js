b6915bca4d85ca3ec94216edb68e1b1a8531b5bd
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-11-12 00:07:35 -0300

    att

--- src/api.js
+++ src/api.js
@@ -4,11 +4,32 @@ const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+
+app.use(cors({
+    origin: '*'
+}))
+
+require('dotenv').config()
+
+
+app.get(':endpoint([\\/\\w\\.-]*)', function (req, res) {
+    // Remove any trailing slash from base url
+    const endpoint = (process.env.API_BASE_URL).replace(/\/$/, "") + req.params.endpoint
+    
+    axios.get(endpoint).then(response => {
+        res.json(response.data)
+    }).catch(error => {
+        res.json(error)
+    })
+})
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express Reverse Proxy Correios Running!");
   });
-});
 
 app.use(`/.netlify/functions/api`, router);
 
